38d0790a3fd5599e9434c13005a29d3183b255d0
Your Name <you@example.com>
2025-12-01 23:30:50 +0800

    change password

--- runtimes/controllers/user/info.go
+++ runtimes/controllers/user/info.go
@@ -89,3 +89,66 @@ func Remove(c *gin.Context) {
 	}
 	response.Success(c, gin.H{"status": "ok"}, "Success")
 }
+
+type RePwdStruct struct {
+	OldPwd string `json:"old_pwd" form:"old_pwd"`
+	NewPwd string `json:"new_pwd" form:"new_pwd"`
+	RePwd  string `json:"re_pwd" form:"re_pwd"`
+}
+
+// 修改密码
+func RePassword(c *gin.Context) {
+	rm := new(RePwdStruct)
+	if err := c.ShouldBind(rm); err != nil {
+		response.Error(c, http.StatusBadRequest, i18n.T("Error"), nil)
+		return
+	}
+
+	if rm.NewPwd == "" {
+		response.Error(c, http.StatusBadRequest, i18n.T("The new password cannot be empty"), nil)
+		return
+	}
+	if rm.NewPwd != rm.RePwd {
+		response.Error(c, http.StatusBadRequest, i18n.T("Different passwords"), nil)
+		return
+	}
+
+	u, ok := c.Get("_user")
+	if !ok {
+		response.Error(c, http.StatusBadRequest, i18n.T("Please Login first"), nil)
+		return
+	}
+
+	user, ok := u.(*admins.Admin)
+	if !ok {
+		response.Error(c, http.StatusBadRequest, i18n.T("Please Login first"), nil)
+		return
+	}
+
+	if user.Password != "" && rm.OldPwd == "" {
+		response.Error(c, http.StatusBadRequest, i18n.T("The original password cannot be empty"), nil)
+		return
+	}
+	if funcs.VerifyPassword(user.Password, rm.OldPwd) != nil {
+		response.Error(c, http.StatusBadRequest, i18n.T("The original password error"), nil)
+		return
+	}
+
+	npwd, err := funcs.GenPassword(rm.NewPwd, 0)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+	user.Password = npwd
+	if err := user.Save(nil); err != nil {
+		response.Error(c, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+
+	jwt, err := user.GenJwt()
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+	response.Success(c, jwt, "success")
+}
